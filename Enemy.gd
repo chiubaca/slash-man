f82156a516f2580e6370f8243ece32c45a2214b6
chiubaca <alexchiu11@gmail.com>
2024-08-18 18:57:24 +0100

    update enemy colour when hit

--- Enemy.gd
+++ Enemy.gd
@@ -1,5 +1,16 @@
 extends CharacterBody3D
 
 
+var mesh
+
+func _ready():
+	mesh = get_node('MeshInstance3D')
+	mesh.visible = false
+
+
 func hit_by_player():
+	mesh.visible = true
+	get_tree().create_timer(0.5).timeout.connect(func(): mesh.visible = false)
 	print('ow you hit me')
+	
+	
