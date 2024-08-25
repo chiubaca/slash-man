0fadcb34ab84bba2547aaf364ade17078598220d
chiubaca <alexchiu11@gmail.com>
2024-08-25 10:51:46 +0100

    attack state working

---
+++ entities/KnightMan/states/Attack.gd
@@ -0,0 +1,14 @@
+extends State
+
+@export
+var idle_state: State
+
+func enter() -> void:
+	parent.animations.play("attack_down")
+
+func process_physics(delta: float) -> State:
+	
+	if not parent.animations.is_playing():
+		return idle_state
+		
+	return self
--- entities/KnightMan/states/Idle.gd
+++ entities/KnightMan/states/Idle.gd
@@ -3,6 +3,9 @@ extends State
 @export
 var run_state: State
 
+@export
+var attack_state: State
+
 func enter() -> void:
 	parent.animations.play('idle')
 
@@ -10,6 +13,9 @@ func enter() -> void:
 func process_input(event: InputEvent)-> State:
 	if event.is_action_pressed("move_right") or event.is_action_pressed("move_left") or event.is_action_pressed("move_back") or event.is_action_pressed("move_forward"): 
 		return run_state
-	
+		
+	if event.is_action_pressed("attack"):
+		return attack_state
+		
 	return self 
 	
